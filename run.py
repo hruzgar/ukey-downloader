f360bd5101fae431a3d49f20c37c93d2d745a52c
Hasan Rüzgar <hasanruzweb@gmail.com>
2022-01-31 23:14:14 +0100

    starting out the project

---
+++ run.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+PATH = "C:\prog\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+def main():
+	log_in()
+	try:
+		dersler_div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "metro")))
+		
+	except:
+		driver.quit()
+	print(dersler_div.text)
+	#dersler_div = driver.find_element(By.CLASS_NAME, "metro")
+	
+
+
+
+
+
+
+def log_in():
+	driver.get("https://ukey.uludag.edu.tr")
+	username = driver.find_element(By.ID, "KullaniciKodu")
+	pw = driver.find_element(By.ID, "sifre")
+	check_student = driver.find_element(By.XPATH, "//input[@value='Student']");
+	username.send_keys("032190148")
+	pw.send_keys("GuzSifre99")
+	check_student.click()
+	pw.send_keys(Keys.RETURN)
+	# We should be inside Ukey on our Homepage right now
+
+
+if __name__ == "__main__":
+	main()
