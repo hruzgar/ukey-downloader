03d77af0e0436f9459bdd59b8c805d9a0cf5cadd
Hasan Rüzgar <hasanruzweb@gmail.com>
2022-02-01 09:55:21 +0100

    struggling since 3 hourse to download files inside script
    site don't let you download unless you make the request with the right headers

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 #using vscode-env envoirement
-from selenium import webdriver
+import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from seleniumwire import webdriver
 import time
+import requests
+import re
 
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
 
 PATH = "C:\\prog\\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome(PATH, options=chrome_options)
 class_links = [];
 
 def main():
@@ -18,6 +24,11 @@ def main():
 	except:
 		print("Please check you Internet connection!")
 		driver.quit()
+
+	for request in driver.requests:
+		if(request.url == "https://ukey.uludag.edu.tr/Images/ukeyuser.jpg"):
+  			print(request.url) # <--------------- Request url
+  			print(request.headers) #
 
 
 	for my_class in classes:
@@ -53,7 +64,17 @@ def download_for_current_class(link_of_class):
 	try:
 		download_links = WebDriverWait(driver, 1).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "dosya")))
 		for down_link in download_links:
-			down_link.click()		
+			link = down_link.get_attribute("href")
+			print(link)
+			#for request in driver.requests:
+			#	if(request.url == "https://ukey.uludag.edu.tr/Ogrenci/DersMateryalleri"):
+  			#		print(request.url) # <--------------- Request url
+  			#		print(request.headers) #
+			# r = requests.get(link, allow_redirects=True)
+			# print(r)
+			# filename = getFilename_fromCd(r.headers.get('content-disposition'))
+			# print(filename)
+			# down_link.click()		
 	except:
 		print("No Download on this Page!")
 		# driver.quit()
@@ -62,6 +83,16 @@ def download_for_current_class(link_of_class):
 	driver.back()
 	driver.back()
 
+def getFilename_fromCd(cd):
+	"""
+	Get filename from content-disposition
+	"""
+	if not cd:
+		return None
+	fname = re.findall('filename=(.+)', cd)
+	if len(fname) == 0:
+		return None
+	return fname[0]
 
 if __name__ == "__main__":
 	main()
---
+++ test_request.py
@@ -0,0 +1,30 @@
+import requests
+import re
+
+
+def getFilename_fromCd(cd):
+	"""
+	Get filename from content-disposition
+	"""
+	if not cd:
+		return None
+	fname = re.findall('filename=(.+)', cd)
+	if len(fname) == 0:
+		return None
+	return fname[0]
+
+
+url = "https://ukey.uludag.edu.tr/p!JVs3XC0vDwtaJlRMYFQsWTIwWCoqITAEAF1ZQ10vN3slLkJgUFwIASdcdAxzdAJvDXsIBmsDIAEgb1YidHNvDiBUUVYIJHcOcCADYA!/"
+headers = {"Cookie":"BIGipServerukey.app~ukey_pool=687936684.20480.0000; _KEY_Rses=41b2140402ae44f2b370d50aa38b76a2; _KEY_Proxy=A0249E87B28B2691DC1775A7E6549065A58903FE",
+	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+	"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+	"Host":"ukey.uludag.edu.tr"}
+
+
+# url="https://speed.hetzner.de/100MB.bin"
+r = requests.get(url, headers=headers, allow_redirects=True)
+# filename = getFilename_fromCd(r.headers.get('content-disposition'))
+with open("test3.ppt", "wb") as f:
+	for chunk in r.iter_content(chunk_size=128):
+		f.write(chunk)
+print(filename)
