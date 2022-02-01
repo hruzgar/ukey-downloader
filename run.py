9a8396c70af3f06797348ba121445f2ea2eb2491
Hasan Rüzgar <hasanruzweb@gmail.com>
2022-02-01 16:19:30 +0100

    added login screen and performance increasements

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-#using vscode-env envoirement
 import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 from seleniumwire import webdriver
 import time
 import requests
@@ -12,18 +13,23 @@ import sys
 import os.path
 import mimetypes
 
+chromedriver_PATH = "chrome/cdriver/chromedriver.exe"
+chrome_PATH = "chrome/App/Chrome-bin/chrome.exe"
+
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--ignore-certificate-errors')
-# chrome_options.add_argument('--ignore-ssl-errors')
-# chrome_options.add_argument('--disable-gpu')
-# chrome_options.add_argument('--disable-software-rasterizer')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--ignore-ssl-errors')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-software-rasterizer')
+chrome_options.add_argument('--incognito')
+chrome_options.binary_location = chrome_PATH
 
 session = requests.Session()
 
-PATH = "C:\\prog\\chromedriver.exe"
-driver = webdriver.Chrome(PATH, options=chrome_options)
+
+driver = webdriver.Chrome(executable_path=chromedriver_PATH, options=chrome_options, desired_capabilities=DesiredCapabilities.CHROME)
 class_links = [];
 headers = {"Host":"ukey.uludag.edu.tr",
 	"Connection":"keep-alive",
@@ -61,11 +67,12 @@ def main():
 		class_name = my_class.find_element(By.TAG_NAME, "a").text
 		class_links.append((class_link, class_name))
 
-	print(class_links)
-
+	print("Lütfen bekle! Birinci indirme 20 saniye sürüyor. Sonra hizlanacak\n")
 	for link, name in class_links:
 		# iterates through all the classes and downloads
 		download_for_current_class(link, name)
+
+	print("Süpeeer! Indime basarili!\nindirilen dosyalari 'Downloads' klasorunde bulabilirsiniz.\nHayirli calismalar ve iyi günler (:")
 	
 	
 	
@@ -74,10 +81,16 @@ def main():
 
 
 def log_in():
+	# student_num = input("\n\nLütfen ögrenci numaranizi giriniz: ")
+	# passw = input("Lütfen sifrenizi giriniz: ")
+	print("\nGiris yapiliyor... (20 saniye sürüyor. Lütfen bekle)\n")
 	driver.get("https://ukey.uludag.edu.tr")
+	print("\nGiris yapildi!\n")
 	username = driver.find_element(By.ID, "KullaniciKodu")
 	pw = driver.find_element(By.ID, "sifre")
 	check_student = driver.find_element(By.XPATH, "//input[@value='Student']");
+	
+
 	username.send_keys("032190148")
 	pw.send_keys("GuzSifre99")
 	check_student.click()
@@ -90,20 +103,28 @@ def download_for_current_class(link_of_class, name_of_class):
 	try:
 		tr_list = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.TAG_NAME, "tbody"))).find_elements(By.TAG_NAME, "tr")
 		for tr in tr_list:
+			diger = True
 			td_list = tr.find_elements(By.TAG_NAME, "td")
 			for td in td_list:
+				if(td.text == "Haftalık Ders Notu"):
+					diger = False
+
 				if(td.text == "Dosyayı Aç"):
 					class_n = [to_ascii(x) for x in name_of_class.split(" - ")]
 					dest_dir = os.path.expandvars('%userprofile%/Downloads/ukey-download/' + "-".join(class_n[1].split(" ")))
 					if not os.path.isdir(dest_dir):
 						os.mkdir(dest_dir)
-					link = td.find_element(By.TAG_NAME, "a").get_attribute("href")
 
-					name_list = tr.text.split(" ")[:-4]
+					link = td.find_element(By.TAG_NAME, "a").get_attribute("href")
+					if diger == True:
+						name_list = tr.text.split(" ")[:-4]
+					else:
+						name_list = tr.text.split(" ")[:-6]
+						
 					name_str = to_ascii("-".join(name_list))
 					week_num = tr.text.split(" ")[-3]
 
-					print("\nDownloading: " + week_num + "_" + class_n[0] + "_" + name_str)
+					print("\nDownloading...\n" + week_num + "_" + class_n[0] + "_" + name_str)
 					start = time.time()
 					# Get Request
 					r = session.get(link, allow_redirects=True, headers=headers)
@@ -136,24 +157,15 @@ def get_cookies():
 			headers["Cookie"] = request.headers["Cookie"]
 			print("\nusing Cookies:",request.headers["Cookie"], "\n\n\n") 
 
-def getFilename_fromCd(cd):
-	"""
-	Get filename from content-disposition
-	"""
-	if not cd:
-		return None
-	fname = re.findall('filename=(.+)', cd)
-	if len(fname) == 0:
-		return None
-	return fname[0]
 
 def to_ascii(my_str):
 	new_ascii = my_str.replace('\u0130', 'I').replace('\u0131', 'i')
-	new_ascii = new_ascii.replace('\u011e', 'G').replace('\u011f', 'g')
-	new_ascii = new_ascii.replace('\u015e', 'S').replace('\u015f', 's')
-	new_ascii = new_ascii.replace('\u00E7', 'c').replace('\u00C7', 'C')
-	new_ascii = new_ascii.replace('ü', 'u').replace('Ü', 'U')
-	new_ascii = new_ascii.replace('ö', 'o').replace('Ö', 'O')
+	new_ascii = new_ascii.replace('\u011E', 'G').replace('\u011F', 'g')
+	new_ascii = new_ascii.replace('\u015E', 'S').replace('\u015F', 's')
+	new_ascii = new_ascii.replace('\u00C7', 'C').replace('\u00E7', 'c')
+	new_ascii = new_ascii.replace('\u00DC', 'U').replace('\u00FC', 'u')
+	new_ascii = new_ascii.replace('\u00D6', 'O').replace('\u00F6', 'o')
+	new_ascii = new_ascii.replace('/', '_').replace('\\', '_')
 	return new_ascii
 
 if __name__ == "__main__":
--- test_request.py
+++ test_request.py
@@ -1,30 +1,43 @@
+import selenium
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from seleniumwire import webdriver
 import requests
 import re
+import time
 
 
-def getFilename_fromCd(cd):
-	"""
-	Get filename from content-disposition
-	"""
-	if not cd:
-		return None
-	fname = re.findall('filename=(.+)', cd)
-	if len(fname) == 0:
-		return None
-	return fname[0]
+chromedriver_PATH = "chrome/cdriver/chromedriver.exe"
+chrome_PATH = "chrome/App/Chrome-bin/chrome.exe"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--incognito')
+chrome_options.binary_location = chrome_PATH
+
+session = requests.Session()
 
 
-url = "https://ukey.uludag.edu.tr/p!IFNGXVYrBX4sVl06FFVWXzAxXltbUDMKBllbQFxeQngmWEwfWVd7ICJSUwAMIFwacFNech8CW1R1blMCAgVsBSZSAlEAUQUGJQAAHw!/"
-headers = {"Cookie":"BIGipServerukey.app~ukey_pool=687936684.20480.0000; _KEY_Rses=41b2140402ae44f2b370d50aa38b76a2; _KEY_Proxy=A0249E87B28B2691DC1775A7E6549065A58903FE",
+driver = webdriver.Chrome(executable_path=chromedriver_PATH, options=chrome_options, desired_capabilities=DesiredCapabilities.CHROME)
+
+url = "https://stackoverflow.com/questions/32817962/selenium-not-starting-portable-chrome-but-local-installation"
+headers = {"Cookie":"BIGipServerukey.app~ukey_pool=553718956.20480.0000; _ga=GA1.3.1998165752.1643706547; _gid=GA1.3.1230881967.1643706547; _gat=1; _KEY_Rses=89596ccd64764a5abe01c90ca67fd494; _KEY_Proxy=F477D79A0AC5F83FD742C8C03BB4B0B435C18D88",
 	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
 	"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 	"Host":"ukey.uludag.edu.tr"}
 
 
 # url="https://speed.hetzner.de/100MB.bin"
-r = requests.get(url, headers=headers, allow_redirects=True)
+r = driver.get("https://ukey.uludag.edu.tr/Home?ReturnUrl=%2f")
+time.sleep(5)
 # filename = getFilename_fromCd(r.headers.get('content-disposition'))
+"""
 with open("test3.ppt", "wb") as f:
 	for chunk in r.iter_content(chunk_size=128):
 		f.write(chunk)
 print(filename)
+"""
+print(r.text)
