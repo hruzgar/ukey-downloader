10ebf89061b1ff7b8e83dfc9b95dae6947926ef3
Hasan Rüzgar <hasanruzweb@gmail.com>
2022-02-01 22:14:05 +0100

    fully working downloader

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import selenium
+import seleniumwire
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
