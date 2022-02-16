3041103db4909f42dd488140fd471a2a598dc25f
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2022-02-17 02:04:01 +0300

    Clean up job: rename variables, format document and delete unnecessary lines

--- run.py
+++ run.py
@@ -1,18 +1,14 @@
-import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 import time
 import requests
-import re
 import sys
 import os.path
-
 
 extension_dict = {
     'application/msword': '.doc',
@@ -34,31 +30,31 @@ extension_dict = {
     'application/vnd.ms-powerpoint.template.macroEnabled.12': '.potm',
     'application/vnd.ms-powerpoint.slideshow.macroEnabled.12': '.ppsm',
     'application/vnd.ms-access': '.mdb',
-    'application/pdf':'.pdf'
+    'application/pdf': '.pdf'
 }
 
-    
-def get_driver():
 
+def get_driver():
     # Sets the path to chrome and chromedriver executables
     if sys.platform == "win32":
         chromedriver_service = Service("chrome/cdriver/chromedriver.exe")
-        chrome_PATH = "chrome/App/Chrome-bin/chrome.exe"
+        path_chrome = "chrome/App/Chrome-bin/chrome.exe"
     elif sys.platform == "linux":
         chromedriver_service = Service("/usr/bin/chromedriver")
         if os.path.exists("/usr/bin/google-chrome"):
-            chrome_PATH = "/usr/bin/google-chrome"
+            path_chrome = "/usr/bin/google-chrome"
         elif os.path.exists("/usr/bin/chromium"):
-            chrome_PATH = "/usr/bin/chromium"
+            path_chrome = "/usr/bin/chromium"
         else:
-            chrome_PATH = input("ukey-downloader chrome kurulumunu bulamadı. lütfen chrome çalıştırılabilir dosyasının tam konumunu giriniz: ")
-            if not os.path.exists(chrome_PATH):
-                print("böyle bir dosya yok ki :( script'i kapatıyorum.")
+            path_chrome = input('''ukey-downloader Chrome kurulumunu bulamadı. 
+            Lütfen chrome çalıştırılabilir dosyasının tam konumunu giriniz: ''')
+            if not os.path.exists(path_chrome):
+                print("böyle bir dosya yok ki :(")
                 sys.exit()
 
     # chrome settings for selenium
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless') # if activated browser is not shown to the end-user
+    chrome_options.add_argument('--headless')  # if activated browser is not shown to the end-user
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--ignore-certificate-errors')
@@ -66,21 +62,22 @@ def get_driver():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-software-rasterizer')
     chrome_options.add_argument('--incognito')
-    chrome_options.binary_location = chrome_PATH
+    chrome_options.binary_location = path_chrome
 
-    return webdriver.Chrome(service=chromedriver_service, options=chrome_options, desired_capabilities=DesiredCapabilities.CHROME)
+    return webdriver.Chrome(service=chromedriver_service, options=chrome_options,
+                            desired_capabilities=DesiredCapabilities.CHROME)
 
 
 def log_in(driver):
     student_num = input("Lütfen ögrenci numaranizi giriniz: ")
     passw = input("Lütfen sifrenizi giriniz: ")
 
-    print("\nGiris yapiliyor...\n") # \n\nTüm indirme yaklasik olarak 5 dakika sürecek. Git kendine bir kahve yap (;\n
-    
+    print("\nGiris yapiliyor...\n")
+
     username = driver.find_element(By.ID, "KullaniciKodu")
     pw = driver.find_element(By.ID, "sifre")
-    check_student = driver.find_element(By.XPATH, "//input[@value='Student']");
-    
+    check_student = driver.find_element(By.XPATH, "//input[@value='Student']")
+
     username.clear()
     pw.clear()
 
@@ -89,8 +86,9 @@ def log_in(driver):
     check_student.click()
     pw.send_keys(Keys.RETURN)
     try:
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CLASS_NAME, "metro"))).find_elements(By.TAG_NAME, "li")
-        return True # We should be on the Ukey Homepage now
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CLASS_NAME, "metro"))).find_elements(
+            By.TAG_NAME, "li")
+        return True  # We should be on the Ukey Homepage now
     except:
         return False
 
@@ -124,26 +122,27 @@ def get_time_dif(start):
     return seconds + "." + mili_seconds + "s"
 
 
-def download_for_current_class(driver, session, link_of_class, name_of_class): 
+def download_for_current_class(driver, session, link_of_class, name_of_class):
     driver.get(link_of_class)
     driver.get("https://ukey.uludag.edu.tr/Ogrenci/DersMateryalleri")
     try:
-        tr_list = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.TAG_NAME, "tbody"))).find_elements(By.TAG_NAME, "tr")
+        tr_list = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.TAG_NAME, "tbody"))).find_elements(
+            By.TAG_NAME, "tr")
         for tr in tr_list:
             diger = True
             td_list = tr.find_elements(By.TAG_NAME, "td")
             for td in td_list:
-                if(td.text == "Haftalık Ders Notu"):
+                if td.text == "Haftalık Ders Notu":
                     diger = False
 
-                if(td.text == "Dosyayı Aç"):
+                if td.text == "Dosyayı Aç":
                     class_n = [to_ascii(x) for x in name_of_class.split(" - ")]
                     dest_dir = destination_folder + "-".join(class_n[1].split(" "))
                     if not os.path.isdir(dest_dir):
                         os.mkdir(dest_dir)
 
                     link = td.find_element(By.TAG_NAME, "a").get_attribute("href")
-                    if diger == True:
+                    if diger:
                         name_list = tr.text.split(" ")[:-4]
                     else:
                         name_list = tr.text.split(" ")[:-6]
@@ -152,16 +151,16 @@ def download_for_current_class(driver, session, link_of_class, name_of_class):
                     week_num = tr.text.split(" ")[-3]
 
                     print("Downloading...\n" + week_num + "_" + class_n[0] + "_" + name_str)
-                    
+
                     # Get Request
                     start = time.time()
-                    r = session.get(link, allow_redirects=True) #, headers=headers
+                    r = session.get(link, allow_redirects=True)  # , headers=headers
                     print("Get-Request elapsed time:", get_time_dif(start))
                     # gets file extension (for example:".pdf")
                     content_type = r.headers['content-type']
                     f_type = extension_dict[content_type]
                     print("File-Extension:", f_type)
-                    
+
                     # writing to file
                     start = time.time()
                     with open(os.path.join(dest_dir, week_num + "_" + name_str + f_type), "wb") as file:
@@ -171,7 +170,7 @@ def download_for_current_class(driver, session, link_of_class, name_of_class):
 
     except:
         print("\n\nNo Download on this Page!\n")
-    
+
     driver.back()
     driver.back()
 
@@ -185,26 +184,26 @@ def main():
 
     print("\nStarting Browser...\n")
     driver.get("https://ukey.uludag.edu.tr")
-    
+
     password_true = log_in(driver)
-    while(not password_true):
+    while not password_true:
         print("Ögrenci Numarasi veya Sifre hatali! Lütfen tekrar dene.\n")
         password_true = log_in(driver)
-    
+
     print("Giris yapildi!\n")
 
     get_cookies(driver, session)
-    
+
     classes = driver.find_element(By.CLASS_NAME, "metro").find_elements(By.TAG_NAME, "li")
-    class_links = [];
+    class_links = []
     for my_class in classes:
         # finds all class links
         class_link = my_class.find_element(By.TAG_NAME, "a").get_attribute("href")
         class_name = my_class.find_element(By.TAG_NAME, "a").text
         class_links.append((class_link, class_name))
 
-    print("Lütfen Bekle! Birinci indirme 20 saniye sürüyor. Sonra hizlanacak\n")
-    
+    print('Lütfen Bekle! Birinci indirme 20 saniye sürüyor. Sonra hizlanacak\n')
+
     # Set platform-specific global variables
     global destination_folder
     if sys.platform == "win32":
@@ -226,9 +225,9 @@ def main():
     driver.quit()
     print("\nSüpeeer! Indirme basarili!\nIndirilen dosyalari 'Downloads' klasöründe bulabilirsin.")
     print("Indirme Süresi:", get_time_dif(start))
-    print("\nPencereyi kapatabilirsin simdi. 60 saniye icinde kendisi kapatacaktir.\nHayirli calismalar ve iyi günler dilerim (:")
+    print('''\nPencereyi kapatabilirsin simdi. 60 saniye icinde kendisi kapatacaktir.
+    \nHayirli calismalar ve iyi günler dilerim (:''')
     time.sleep(60)
-
 
 
 if __name__ == "__main__":
