c603bd41e87fca0b1a12019878badb148d0a843f
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2022-02-18 19:40:30 +0300

    Set working directory of shell to parent directory of script

--- installer/install.sh
+++ installer/install.sh
@@ -7,7 +7,9 @@ set -e
 steps="6"
 installdir=/usr/local/bin
 desktopfiledir="$HOME/.local/share/applications/"
+scriptdir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
+cd "$scriptdir"
 echo "This script installs ukeydl and related files to $installdir"
 echo "You can change installdir variable to install to a different location"
 echo "But this will break the ukeydl.desktop and ukeydl files, you should edit them first"
@@ -17,7 +19,7 @@ echo "(1/$steps) Creating folder $installdir/ukey-downloader if not created alre
 sudo mkdir --parents $installdir/ukey-downloader
 
 echo "(2/$steps) Copying run.py to $installdir"
-sudo cp ../../run.py $installdir/ukey-downloader/.
+sudo cp ../run.py $installdir/ukey-downloader/.
 
 echo "(3/$steps) Copying ukeydl (runs the python script) to $installdir"
 sudo cp ukeydl $installdir/.
